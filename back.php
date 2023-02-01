4f4443ad44370cf205eb1ebe870c9c512192be9a
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-02-01 11:11:14 -0300

    comecei o site

--- back.php
+++ back.php
@@ -1,10 +1,47 @@
 <?php
+    $name = $_POST['name'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $data = 0;
+    $hora = 0;
 
 
+    $bdhost = "localhost";
+    $bduser = "root";
+    $bdpass = "";
+    $bddata = "trabalho2";
+    
+    // Conexão com a base de dados
+    $conn = mysqli_connect($bdhost, $bduser, $bdpass, $bddata) or die ("could not connect to mysql"); 
+    mysqli_select_db($conn, "trabalho2") or die ("no database");
+    
+    // Inserção dos dados de entrada no banco de dados
+    
+    $sql = "INSERT INTO mensagens (data, hora , nome, telefone, email, mensagem) VALUES ('{$data}', '{$hora}', '{$name}', '{$tel}', '{$email}', '{$message}')";
+    
+    if ($name == null or $tel == 0 or $email == null or $message == null)
+    {
+    echo "Um dos campos vazios , por favor, prencha todas as informações.";
+    echo "{$name} <br>";
+    echo "{$tel} <br>";
+    echo $email;
+    echo $message;
+    }
+    else
+    {
+        /*
+        if (mysqli_query($conn, $sql)) 
+        {
+            echo "Mensagem enviada com sucesso.";
+        } 
+        else 
+        {
+            echo "Erro ao enviar a mensagem.";
+        } */
+        echo "teste";
+    }
 
-
-
-
-
-
+    mysqli_close($conn);
 ?>
--- index.php
+++ index.php
@@ -10,13 +10,19 @@
 </head>
 
 <body>
-    <?php
-        include("back.php");
-    ?>
 
-    <div>
+    <form action="back.php" method="post">
+        <label for="name">Nome:</label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="tel">Telefone:</label>
+        <input type="tel" id="tel" name="tel"><br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br><br>
+        <label for="message">Mensagem:</label>
+        <textarea id="message" name="message"></textarea><br><br>
+        <input type="submit" value="Enviar">
+    </form>
 
-    </div>
 </body>
 
 </html>
